faaf69798dec61a5afa2b4c0bed5267697f79c95
oharko <o.harko@outlook_dev.com>
2016-04-16 01:45:03 +0400

    Showing current tab url in popup.

--- source/js/extCtrl.js
+++ source/js/extCtrl.js
@@ -1,5 +1,8 @@
 (function(angular) {
-angular.module('extApp').controller('extCtrl', ['$scope', function($scope) {
-    $scope.text = 'text from controller!!!';
-}]);
+angular.module('extApp').controller('extCtrl', function($scope) {
+  		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    	$scope.url = tabs[0].url;    
+    	$scope.title = tabs[0].title;
+    	$scope.$apply();});  
+});
 })(window.angular);
